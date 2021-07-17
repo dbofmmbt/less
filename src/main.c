9760eea49be18758f6ff4649d03ac13af5eb8357
Matheus Marotti <matheus@bgcbrasil.com.br>
2021-07-17 13:33:59 -0300

    Added DataDistribution

--- src/main.c
+++ src/main.c
@@ -78,6 +78,54 @@ void ProcessInitialization(
   }
 };
 
+// Data distribution among the processes
+void DataDistribution(
+  double *pMatrix,
+  double *pProcRows,
+  double *pVector,
+  double *pProcVector,
+
+  int Size, 
+  int RowNum
+) {
+  int *pSendNum = malloc(sizeof(int) * ProcNum); // Number of data sent to the process
+  int *pSendInd = malloc(sizeof(int) * ProcNum); // Index of first row sent to the process
+
+  int RestRows = Size; // Number of rows, that have not been  distributed yet
+
+  RowNum = (Size / ProcNum); // Define the disposition of the matrix rows for the current process
+
+  pSendNum[0] = RowNum * Size;
+  pSendInd[0] = 0;
+
+  for (int i = 1; i < ProcNum; i++) {
+    RestRows -= RowNum;
+    RowNum = RestRows / (ProcNum - i);
+    pSendNum[i] = RowNum * Size;
+    pSendInd[i] = pSendInd[i-1] + pSendNum[i-1];
+  }
+
+  // Scatter the rows
+  MPI_Scatterv(pMatrix, pSendNum, pSendInd, MPI_DOUBLE, pProcRows, pSendNum[ProcRank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+  // Define the disposition of the matrix rows for current process
+  RestRows = Size;
+  pProcInd[0] = 0;
+  pProcNum[0] = Size / ProcNum;
+
+  for (int i = 1; i < ProcNum; i++) {
+    RestRows -= pProcNum[i-1];
+    pProcNum[i] = RestRows/(ProcNum-i);
+    pProcInd[i] = pProcInd[i-1]+pProcNum[i-1];
+  }
+
+  // Scatter the vector rows
+  MPI_Scatterv(pVector, pProcNum, pProcInd, MPI_DOUBLE, pProcVector, pProcNum[ProcRank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+  free(pSendNum);
+  free(pSendInd);
+}
+
 int main(int argc, char* argv[]) {
   double *pMatrix; // Matrix of the linear system
   double *pVector; // Right parts of the linear system
@@ -105,6 +153,8 @@ int main(int argc, char* argv[]) {
     PrintMatrixAndVector(pMatrix, pVector, Size);
   }
   
+  DataDistribution(pMatrix, pProcRows, pVector, pProcVector, Size, RowNum);
+
   MPI_Finalize();
 
   return 0;
